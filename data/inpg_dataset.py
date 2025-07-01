SANE Test -> train leakage is fixed

data/inpg_dataset.py
@@ -110,7 +110,7 @@ class GridEncoder():
     
 
 class IngpData(Dataset):
-    def __init__(self, data_path, config, window_size = None, token_size = None, max_layer_width = 64, device = None):
+    def __init__(self, data_path, config, window_size = None, token_size = None, max_layer_width = 64, device = None, train = True):
         self.data_path = data_path
         self.config = config
 
@@ -136,6 +136,15 @@ class IngpData(Dataset):
         #EXP1
         self.max_positions = torch.tensor([self.grid_encoder.hash_table_indices_end[-1]+1 ,54])
 
+        self.train = train
+        len_train = int(0.85 * len(self.all_objects_2d))
+        len_test = len(self.all_objects_2d) - len_train
+        if self.train:
+            self.all_objects_2d = self.all_objects_2d[:len_train]
+        else:
+            self.all_objects_2d = self.all_objects_2d[len_train:]
+
+
     def __len__(self):
         return len(self.all_objects_2d) * 20000
     
@@ -144,7 +153,7 @@ class IngpData(Dataset):
         #Random index
         #Sample (window_size - hash_chunk_size )points in 3D space (512,3)
         points1 = torch.rand((self.hash_chunk_size, 3))
-        points2 = torch.rand((self.hash_chunk_size, 3))
+        #points2 = torch.rand((self.hash_chunk_size, 3))
         
         #Get 2 random views of the object
         object_parent_path = self.all_objects_2d[idx]
@@ -154,7 +163,7 @@ class IngpData(Dataset):
         obj2_path, obj_2_transform = object_parent_path[idx_child[1]] 
 
         [t1,p1,m1], r1 = self.load_weights(obj1_path, points1), obj_1_transform
-        (t2,p2,m2), r2 = self.load_weights(obj2_path, points2), obj_2_transform
+        (t2,p2,m2), r2 = self.load_weights(obj2_path, points1), obj_2_transform
 
         return t1, p1, m1, r1, t2, p2, m2, r2
     

models/cv/sane.py
@@ -8,8 +8,8 @@ import torch.nn as nn
 from deeppy.utils import print_args
 
 from deeppy import Network, SqueezeLastDimention, QuaternionLoss, NT_Xent, Optimizer
-from deeppy import LinearTokenizerBeforePosition
-from deeppy import SaneXYZPositionalEmbedding, SanePositionalEmbedding
+from deeppy import SaneLinearTokenizerBeforePosition
+from deeppy import SaneXYZPositionalEmbedding
 from deeppy.models import BaseModel
 
 class Sane(BaseModel):
@@ -72,9 +72,10 @@ class Sane(BaseModel):
 		z = self.autoencoder.encode((X,p))
 
 		#z[:,0,4:] = 0
-		zp = self.project(z[:,1:])
+		zp = self.project(z[:,:-1])
+		z_rot = self.classify(z[:,-1, :])
 		y = self.autoencoder.decode((z,p))
-		return z, y, zp
+		return z, y, zp, z_rot
 
 	def encode(self,X):
 		return self.autoencoder.encode(X)
@@ -95,8 +96,8 @@ class Sane(BaseModel):
 		x_1, p_1,m_1,r_1, x_2, p_2,m_2,r_2 = X
 		r_1, r_2 = self.rot_crit.euler_to_quaternion(r_1), self.rot_crit.euler_to_quaternion(r_2)
 
-		z_1, y_1, zp_1 = self((x_1, p_1))
-		z_2, y_2, zp_2 = self((x_2, p_2))
+		z_1, y_1, zp_1, z_rot_1 = self((x_1, p_1))
+		z_2, y_2, zp_2, z_rot_2 = self((x_2, p_2))
 		
 		#Compute reconstruction loss
 		x = torch.cat([x_1, x_2], dim=0)
@@ -105,9 +106,7 @@ class Sane(BaseModel):
 		recon_loss = self.recon_crit(y*m,x)
 		
 		#Compute rotation loss
-		z_rot1 = self.classify(z_1[:,0,:]) #[B_size x 4]
-		z_rot2 = self.classify(z_2[:,0,:]) #[B_size x 4]
-		rot_loss = self.rot_crit(z_rot1, z_rot2, r_1, r_2)
+		rot_loss = self.rot_crit(z_rot_1, z_rot_2, r_1, r_2)
 
 		#Compute NTX loss
 		ntx_loss = self.ntx_crit(zp_1, zp_2)
@@ -126,7 +125,7 @@ class Sane(BaseModel):
 		encoder = nn.TransformerEncoderLayer(d_model = self.embed_dim, nhead= self.num_heads, dim_feedforward = 4* self.embed_dim, batch_first= True, norm_first = True, dropout=self.dropout, bias= self.bias, activation = nn.GELU())
 		decoder = nn.TransformerEncoderLayer(d_model = self.embed_dim, nhead= self.num_heads, dim_feedforward = 4* self.embed_dim, batch_first= True, norm_first = True, dropout=self.dropout, bias= self.bias, activation = nn.GELU())
 		
-		blocks = [LinearTokenizerBeforePosition,SaneXYZPositionalEmbedding, nn.Dropout, nn.TransformerEncoder, nn.Linear]
+		blocks = [SaneLinearTokenizerBeforePosition,SaneXYZPositionalEmbedding, nn.Dropout, nn.TransformerEncoder, nn.Linear]
 		encoder_params = {
 			"blocks": blocks,
 			"block_args":[

modules/positional_embedding.py
@@ -23,11 +23,10 @@ class SaneLinearTokenizerBeforePosition(nn.Module):
         self.divide_ix = max_positions[1]
         self.linear_hash= nn.Linear(in_features,out_features)
         self.linear_mlp = nn.Linear(in_features,out_features)
-        self.position = nn.Embedding(1, out_features)
     def forward(self,X):
         x,p = X
-        hash,mlp = x[:,:-self.divide_ix],  x[:, -self.divide_ix:-1]
-        return torch.cat((self.linear_hash(hash), self.linear_mlp(mlp), self.position(0)), dim=1)
+        hash,mlp = x[:,:-self.divide_ix],  x[:, -self.divide_ix:]
+        return torch.cat( (self.linear_hash(hash), self.linear_mlp(mlp)), dim=1),p
 
 class ChunkwiseLinearTokenizer(nn.Module):
     def __init__(self, chunk_size, out_dim):